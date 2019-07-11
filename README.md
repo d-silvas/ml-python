c6faff67813f993d0f7ab48c1dc399c025867803
David Silva <d.silvas@outlook.com>
2019-07-11 21:23:28 +0100

    Scaling data; Fixed pylint

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # Python ML course
+- As seen in [https://www.udemy.com/machinelearning/](https://www.udemy.com/machinelearning/)
 - Visit [https://www.superdatascience.com/pages/machine-learning](https://www.superdatascience.com/pages/machine-learning) to get the data set
+
+## Requirements
+- Python 3.7
+- (Recommended) VSCode
 
 ## Installation
 ```
@@ -10,6 +15,10 @@ pip install -r requirements.txt
 ```
 
 ## Notes
+- Before running, remember to
+```
+source ./venv/bin/activate
+```
 - Execute the file and leave the Python interpreter open after the execution (to check variables etc):
 ```
 python -i lesson.py
--- machine_learning_a_z/part_01_data_preprocessing/section_02/v21_splitting_dataset.py
+++ machine_learning_a_z/part_01_data_preprocessing/section_02/v21_splitting_dataset.py
@@ -2,7 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# See v19categorical_data.py
+###############################
+# v19categorical_data.py
 dataset = pd.read_csv('Data.csv')
 
 x = dataset.iloc[:, :-1].values
@@ -26,4 +27,11 @@ x = onehotencoder.fit_transform(x).toarray()
 
 labelencoder_y = LabelEncoder()
 y = labelencoder_y.fit_transform(y)
+###############################
 
+# Split the dataset into the Trainig set and the #test set
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(
+  x, y, test_size=0.2, # we have 20% of observations in test set
+  random_state=0
+)
